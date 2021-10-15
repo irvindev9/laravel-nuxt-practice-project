92deb3982d0493a96587d07edf8f5b2ad35cd0bd
Irvin López <irvin_21_95@hotmail.com>
2021-10-14 23:53:27 -0600

    product

--- database/migrations/2021_10_14_041633_create_products_table.php
+++ database/migrations/2021_10_14_041633_create_products_table.php
@@ -15,6 +15,10 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->string('image');
+            $table->decimal('price');
             $table->timestamps();
         });
     }
