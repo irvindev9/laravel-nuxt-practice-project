5d321772b0a48a88c2cb9d2f10718b41e9867d95
Irvin López <irvin_21_95@hotmail.com>
2021-10-24 08:48:50 -0600

    Order and order items

--- app/Models/Link.php
+++ app/Models/Link.php
@@ -12,10 +12,27 @@ use Illuminate\Database\Eloquent\Model;
  * @method static \Illuminate\Database\Eloquent\Builder|Link newQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|Link query()
  * @mixin \Eloquent
+ * @property int $id
+ * @property string $code
+ * @property int $user_id
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Database\Factories\LinkFactory factory(...$parameters)
+ * @method static \Illuminate\Database\Eloquent\Builder|Link whereCode($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Link whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Link whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Link whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Link whereUserId($value)
+ * @property-read \App\Models\User $user
  */
 class Link extends Model
 {
     use HasFactory;
 
     protected $guarded = [];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
---
+++ app/Models/OrderItem.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * App\Models\OrderItem
+ *
+ * @method static \Illuminate\Database\Eloquent\Builder|OrderItem newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|OrderItem newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|OrderItem query()
+ * @mixin \Eloquent
+ * @method static \Database\Factories\OrderItemFactory factory(...$parameters)
+ */
+class OrderItem extends Model
+{
+    use HasFactory;
+
+    protected $guarded = [];
+}
